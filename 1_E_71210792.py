11e98a4012c8a0de044c6c9e4586ca516c8098b0
IndyExodio <93326211+IndyExodio@users.noreply.github.com>
2021-11-12 12:06:06 +0800

    Add files via upload

---
+++ 1_E_71210792.py
@@ -0,0 +1,7 @@
+a = int (input("Masukkan alas atas : "))
+d = int (input("Masukkan alas bawah : "))
+i = int (input("Masukkan tinggi : "))
+
+L = 1/2 * (a + d) * i
+
+print ("Luas trapesium adalah : ",L)
---
+++ 2_E_71210792.py
@@ -0,0 +1,8 @@
+name = input("Nama : ")
+lahir = input("Tempat tanggal lahir : ")
+
+a = name.split()
+b = lahir.split()
+
+print("Haloo!", a[1], ",", a[0])
+print("Anda lahir di", b[0], "pada tanggal", b[1], b[2], b[3])
